473103045fff26fdf1a5e90d1ee335c4560e0bcc
Kamran Zafar <kami_ravian@yahoo.com>
2018-03-11 21:26:30 +0000

    fix shelf set initialization

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,7 +1,7 @@
 class VendingMachine
   attr_reader :shelf_set, :change_machine
 
-  def initialize(shelf_set: ShelfSet.new, change_machine: ChangeMachine.new)
+  def initialize(shelf_set: ShelfSet.new([]), change_machine: ChangeMachine.new)
     @shelf_set, @change_machine = shelf_set, change_machine
   end
 end
