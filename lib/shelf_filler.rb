8a308a86a213fa812b12b239479633928baaf8bb
Kamran Zafar <kami_ravian@yahoo.com>
2018-03-11 22:06:26 +0000

    Add shelf filler
    Shelf Filler fills all shelves with
    the given products and returns all
    shelves

--- lib/shelf_filler.rb
+++ lib/shelf_filler.rb
@@ -1,3 +1,16 @@
 class ShelfFiller
+  def fill_all(products)
+    shelves = []
+    products.each_with_index do |product, shelf_number|
+      shelf = prepare_shelf(shelf: Shelf.new, shelf_code: shelf_number, item: product)
+      shelves << shelf
+    end
+    shelves
+   end
 
+   private
+   def prepare_shelf(shelf:, shelf_code:, item:)
+     shelf.code = shelf_code
+     shelf.fill(item)
+   end
 end
--- spec/shelf_filler_spec.rb
+++ spec/shelf_filler_spec.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe ShelfFiller do
+  let(:shelf_filler) { described_class.new }
+  let(:products) { [{ 'Snacks': '50p' }, { 'Water': '70p', quantity: 15 }] }
 
+  describe "#fill_all" do
+    let(:shelf) { double("Shelf") }
+
+    it "populates shelves with given products" do
+      allow(Shelf).to receive(:new).and_return(shelf)
+      allow(shelf).to receive(:code=).and_return("001")
+      allow(shelf).to receive(:fill).and_return(shelf)
+      shelves = shelf_filler.fill_all(products)
+      expect(shelves).to be_a(Array)
+      expect(shelves.length).to eq(2)
+    end
+  end
 end
