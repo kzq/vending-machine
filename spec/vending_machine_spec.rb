b4b8df81d648e49c64998b5b4d89b8963b8fa660
Kamran Zafar <kami_ravian@yahoo.com>
2018-03-11 13:45:41 +0000

    Create vending machine
    It has two components shelf set
    and chnage machine. Vending machine
    can be created with empty or pre populated
    components

---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+RSpec.describe VendingMachine do
+  subject(:machine) { described_class.new }
+
+  describe "initialize" do
+    let(:shelf_set) { double("shelf_set") }
+    let(:change_machine) { double("change_machine") }
+
+    before :each do
+      allow(ShelfSet).to receive(:new).and_return(shelf_set)
+      allow(ChangeMachine).to receive(:new).and_return(change_machine)
+    end
+
+    context "when machine is created with empty components" do
+      it { is_expected.to have_attributes(shelf_set: shelf_set , change_machine: change_machine) }
+    end
+
+    context "when machine is created with pre populated componets" do
+      subject { described_class.new(shelf_set: shelf_set , change_machine: change_machine) }
+
+      it { is_expected.to have_attributes(shelf_set: shelf_set , change_machine: change_machine) }
+    end
+  end
+
+  it "knows about its shelf set" do
+    expect(subject).to respond_to(:shelf_set)
+  end
+
+  it "knows about its attached cash machine" do
+    expect(subject).to respond_to(:change_machine)
+  end
+end
