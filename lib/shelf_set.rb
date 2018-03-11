40e7543aec477557826b6b2fff3eced486749610
Kamran Zafar <kami_ravian@yahoo.com>
2018-03-11 21:29:22 +0000

    Add Shel Set
    It act as chamber that holds all
    shelves in the machine

--- lib/shelf_set.rb
+++ lib/shelf_set.rb
@@ -1,3 +1,15 @@
 class ShelfSet
+  attr_accessor :shelves
 
+  def initialize(shelves)
+    @shelves = shelves
+  end
+
+  def add(shelf:)
+    @shelves.push(*shelf)
+  end
+
+  def find(shelf_code)
+    @shelves.detect { |shelf| shelf.code == shelf_code }
+  end
 end
