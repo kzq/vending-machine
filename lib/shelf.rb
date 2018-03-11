d2d4be63028ca63a8cb50c6e8dc4cd5bae5d2b8f
Kamran Zafar <kami_ravian@yahoo.com>
2018-03-11 23:17:15 +0000

    Add Shelf
    Shelf holds details about the items and keep tracks of the no of items
    it has

--- lib/shelf.rb
+++ lib/shelf.rb
@@ -1,3 +1,35 @@
 class Shelf
+  attr_accessor :code, :quantity, :item
 
+  def initialize
+    @quantity= 0
+  end
+
+  def fill(product)
+   product = defaults.merge(product)
+   @quantity = product[:quantity]
+   item_tuple = extract_item(product)
+   @item = Item.new(item_tuple[0], item_tuple[1])
+  end
+
+  def add_item
+   @quantity+=1
+  end
+
+  def remove_item
+   @quantity-=1 unless @quantity <= 0
+  end
+
+  def defaults
+    { quantity: 20}
+  end
+
+  private
+  def extract_item(product)
+    product.delete(:quantity)
+    product.to_a.flatten
+  end
 end
+
+# item
+Item = Struct.new(:name, :price)
--- spec/shelf_spec.rb
+++ spec/shelf_spec.rb
@@ -1,5 +1,42 @@
 # frozen_string_literal: true
 
 RSpec.describe Shelf do
+  let(:shelf) { Shelf.new }
+  let(:product) { { 'Snacks': '50p', quantity: 10 } }
+  let(:item) { { name: 'Snacks', price: '50p'} }
 
+  describe "#fill" do
+    it "fills the shelf with items" do
+      allow(Item).to receive(:new).with(:Snacks, "50p").and_return(item)
+      shelf.fill(product)
+      expect(shelf.quantity).to eq(10)
+      expect(shelf.item).to eq(item)
+    end
+  end
+
+  it "responds to code" do
+    expect(shelf).to respond_to(:code)
+  end
+
+  it "responds to quantity" do
+      expect(shelf).to respond_to(:quantity)
+  end
+
+  it "responds to item" do
+      expect(shelf).to respond_to(:item)
+  end
+
+  describe "#add_item" do
+    it "add an item to shelf" do
+      shelf.add_item
+      expect(shelf.quantity).to eq(1)
+    end
+  end
+
+  describe "#remove_item" do
+    it "removes an item from the shelf" do
+      shelf.remove_item
+      expect(shelf.quantity).to eq(0)
+    end
+  end
 end
