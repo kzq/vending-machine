a4775aab62b8ab6bf6df6c6af4d5e2ba4f09e519
Kamran Zafar <kami_ravian@yahoo.com>
2018-03-13 23:53:30 +0000

    update interactor

--- irb_interactor.rb
+++ irb_interactor.rb
@@ -1,6 +1,5 @@
 # TODO Encapsulate shelf behaviour into Shelfing module
 # TODO Create Currency interface for all currency types
-# TODO Fix transaction logic
 # TODO Create mixin module for helper methods
 # OPTIMIZE imporive exception managemnt
 # OPTIMIZE improve interactor
@@ -9,11 +8,16 @@
 require './lib/vending_machine'
 
 
+
 products = [{ 'pepsi': '50p', quantity: 10 }, { 'chocolate': '70p' }, {'coca-cola': '60p'}, { 'gum': '10p' }]
 # using stand alone Installer
 
 # installer = Installer.new
 # machine = installer.setup(products: products)
+
+puts "\n\n"
+puts "Setting up your vending machine with the follwoing products\n\n"
+puts "Product =#{products}\n\n"
 
 machine = VendingMachine.new
 
@@ -26,33 +30,58 @@ shelf_set = machine.shelf_set
 # change_machine
 change_machine = machine.change_machine
 
-# product selection codes
-#puts ">>>>>>>>>>>>>>>machine.shelf_set.shelves=#{machine.shelf_set.shelves.inspect}"
-shelf_codes = machine.shelf_set.shelves.map(&:code)
-puts "Product codes (please select one code) = #{shelf_codes}"
-selection = gets.chomp
-shelf = machine.shelf_set.find(selection.to_i)
+puts "Current Stock in hand ......................\n\n"
+puts "#{ shelf_set.shelves.collect { |shelf| { name: shelf.item.name, quantity: shelf.quantity } } }"
+puts "\n\n"
 
+# product selection codes
+shelf_codes = machine.shelf_set.shelves.map(&:code)
+
+puts "SELECTION (please select one code) = #{shelf_codes}\n\n"
+selection = gets.chomp
+puts "\n\n"
+
+shelf = machine.shelf_set.find(selection.to_i)
 product = shelf.item.name
-puts "selected product = #{product}"
 
 product_price = shelf.item.price
-puts "selected product price = #{product_price}"
+puts "Selected product price is = #{product_price}\n\n"
 
-puts "Please insert/p #{product_price}"
-coin = gets.chomp
+puts "Please insert or pay = #{product_price}\n\n"
 
-#puts "value of price = #{GBP.to_value(product_price)}"
-puts "Received = #{coin}"
-puts "value of payment = #{GBP.to_value("#{coin}")}"
+price = GBP.to_value(product_price)
+
+transaction = Transaction.new(charge: price)
+
+while(transaction.status[:info] == "remaining")
+  coin = gets.chomp
+  if GBP.valid?(coin)
+    transaction.process(payment: GBP.to_value(coin))
+    puts "Total amount_paid = #{GBP.to_coin(transaction.amount_paid)}\n\n"
+  else
+    puts "Invalid coin returned = #{coin}\n\n"
+  end
+end
+
+shelf.remove_item
+puts "Here is your product = #{product}\n\n"
+
+puts "PAYMENT DETAILS............\n\n"
+puts "Total payments = #{transaction.payments}"
+puts "Total amount paid = #{transaction.amount_paid}"
+puts "Total balance = #{transaction.balance}"
+puts "Total status = #{transaction.status}\n\n"
+
+puts "Current Stock in hand......................\n\n"
+puts "#{ shelf_set.shelves.collect { |shelf| { name: shelf.item.name, quantity: shelf.quantity } } }"
+puts "\n\n"
+
+puts "Adding money to cash box..."
+
+coins = transaction.payments.collect {|coin_value| GBP.to_coin(coin_value) }
 
 cashier = Cashier.new(change_machine: change_machine)
-transaction_status = cashier.transaction(payment: GBP.to_value("#{coin}") , charge: GBP.to_value(product_price))
-puts "transaction_status = #{transaction_status}"
-#hopper = machine.change_machine.insert_money("20p")
-#puts "Selected hopper = #{hopper.inspect}"
+credited = cashier.credit(money: coins)
 
-puts ">>>>>>CASH====="
+puts "Amount credited = #{credited}\n\n"
 
-#cash = machine.change_machine.hoppers
-#puts "cash = #{cash.inspect}"
